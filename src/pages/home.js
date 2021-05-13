0428044df4336d21e7a382edb56ec5c46708d85a
Riley <rileyjakusik@outlook.com>
2021-05-12 20:25:46 -0500

    Added final presentation video and presentation deck
    == waiting on long doc and role titles ==
    == Anna Gingle working on responsiveness ==

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,19 +8,26 @@ import video from "../assets/icons/Video.svg";
 import link from "../assets/icons/Links.svg";
 
 //documents
-import hold from "../assets/documents/Audience.docx";
-import vid from "../assets/videos/VeerClickthrough.mp4";
+import deck from "../assets/documents/Veer - Deck.pdf";
 
 function Home() {
 	return (
 		<div>
 			<div className="video">
-				<video src={"https://www.youtube.com/watch?v=1oT5qUx8ldw"} controls />
+				<iframe
+					width="1350"
+					height="600"
+					src="https://www.youtube.com/embed/mJeQtW2n4rU"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+				></iframe>
 			</div>
 
 			<div className="grid-home">
-				<Card title="Presentation Deck" icon={pdf} doc={hold} />
-				<Card title="Case Study" icon={pdf} doc={hold} />
+				<Card title="Presentation Deck" icon={pdf} doc={deck} />
+				<Card title="Case Study" icon={pdf} doc={deck} />
 				<Card
 					title="Interactive Prototype"
 					icon={link}
@@ -28,7 +35,11 @@ function Home() {
 						"https://www.figma.com/proto/4QxsgOwL6bbNSwpEEzUoM7/Veer-Prototype-Final-Version?page-id=363%3A0&node-id=363%3A108&viewport=420%2C648%2C0.053420539945364&scaling=scale-down"
 					}
 				/>
-				<Card title="Prototype Video" icon={video} doc={vid} />
+				<Card
+					title="Prototype Video"
+					icon={video}
+					doc={"https://youtu.be/dqiS6nK7n4k"}
+				/>
 			</div>
 		</div>
 	);
